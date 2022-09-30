907ec159520a3ff8a0f175ed57f8f1775b4ef77c
Leyder Quintero <lele.quinter@gmail.com>
2022-09-30 15:53:28 -0500

    Introducción a los Pipes de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre: string = 'Leyder'
+  valor: number = 12123123
+  obj = {
+    nombre: 'fern'
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+  }
 }
