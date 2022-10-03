685c770231727485be0248ce32b5d63ca9f11c8c
Leyder Quintero <lele.quinter@gmail.com>
2022-10-03 12:39:22 -0500

    Cambio en el sortable de vuela

--- src/app/ventas/pipes/ordenar.pipe.ts
+++ src/app/ventas/pipes/ordenar.pipe.ts
@@ -13,7 +13,7 @@ export class OrdenarPipe implements PipeTransform {
         return heroes = heroes.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1);
 
       case 'vuela':
-        return heroes = heroes.sort((a, b) => (a.vuela > b.vuela) ? 1 : -1);
+        return heroes = heroes.sort((a, b) => (a.vuela > b.vuela) ? -1 : 1);
 
       case 'color':
         return heroes = heroes.sort((a, b) => (a.color > b.color) ? 1 : -1);
