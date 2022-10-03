c08600e1f3d1f11de4977b4a3ed22d5d52bef020
Leyder Quintero <lele.quinter@gmail.com>
2022-10-03 12:21:12 -0500

    Ordenar héroes por nombre - OrdenarPipe

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -10,6 +10,7 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
 import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { VuelaPipe } from './pipes/vuela.pipe';
     BasicosComponent,
     OrdenarComponent,
     MayusculasPipe,
-    VuelaPipe
+    VuelaPipe,
+    OrdenarPipe
   ],
   exports: [
     NumerosComponent,
